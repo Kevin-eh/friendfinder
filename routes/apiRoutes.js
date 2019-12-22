cc1359ceb21974c9c6df65223bb2c1ab0def3b03
Kevin-eh <54459190+Kevin-eh@users.noreply.github.com>
2019-12-21 20:04:35 -0800

    Basic functionality I think I don't know. Should have a modal and some fun things. Look friends!

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,12 +6,41 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-    var newFriend = req.body;
+    //got stuck on this part. Used https://github.com/kwoulfe/Friend-Finder/blob/master/app/routing/apiRoutes.js for the for loop.
+    function findUserMatch(answers) {
+      var userSum = 0;
 
-    console.log(newFriend);
+      for (let h = 0; h < answers.length; h++) {
+        userSum += +answers[h];
+      }
+      var bestMatch = {
+        friend: null,
+        lowest: null
+      };
+      for (let i = 0; i < friends.length; i++) {
+        var runningTotal = 0;
+        for (let j = 0; j < friends[i].answers.length; j++) {
+          runningTotal += friends[i].answers[j];
+        }
+        var difference = Math.abs(userSum - runningTotal);
+        if (difference < bestMatch.lowest || bestMatch.friend == null) {
+          bestMatch.friend = friends[i];
+          bestMatch.lowest = difference;
+        }
+      }
+      return bestMatch.friend;
+    }
+    var newMatch = findUserMatch(req.body.answers);
+    console.log(newMatch);
+    friends.push(req.body);
+    res.json(newMatch);
 
-    friends.push(newFriend);
+    // var newFriend = req.body;
 
-    res.json(newFriend);
+    // console.log(newFriend);
+
+    // friends.push(newFriend);
+
+    // res.json(newFriend);
   });
 };
